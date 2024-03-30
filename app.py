ad534bf69612cc4b904d1740489271568ea75284
HaynesChen <chen.ian0713@outlook.com>
2024-03-30 19:27:34 +0800

    Refactor: Changed the format of the database from `.db` to `.sqlite`

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request,jsonify, render_template
+from flask import Flask, request, jsonify, render_template
 from flask_socketio import SocketIO, disconnect, emit
 import random
 import json
 import db.db as db
 from datetime import datetime
+
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
@@ -16,46 +17,58 @@ GAMETIME = 10
 # max round
 MAXROUND = 10
 
+
 # route
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # 查询用户信息
 # return {status:1或0, msg:' ', stuId: 'xxx', times: 1或0}
-@app.route('/api/getuser', methods=['GET'])# calhost:5000/api/getuser?stuId=xxx
+@app.route("/api/getuser", methods=["GET"])  # calhost:5000/api/getuser?stuId=xxx
 def get_connection_status():
-    res = db.check_id(request.args.get('stuId'))
+    res = db.check_id(request.args.get("stuId"))
     if res != None:
-        return jsonify({'status':1, 'msg':'Success', 'stuId':res[1], 'times':res[2], 'score':res[3]})
+        return jsonify(
+            {
+                "status": 1,
+                "msg": "Success",
+                "stuId": res[1],
+                "times": res[2],
+                "score": res[3],
+            }
+        )
     else:
-        return jsonify({'status':0, 'msg':'Invalid', 'stuId':'', 'times':0})
+        return jsonify({"status": 0, "msg": "Invalid", "stuId": "", "times": 0})
+
 
 # get_rank api
-@app.route('/api/getrank', methods=['GET'])
+@app.route("/api/getrank", methods=["GET"])
 def get_rank():
     result = db.get_rank()
     return jsonify(result)
 
-@socketio.on('query')
+
+@socketio.on("query")
 def query_word():
-    
+
     # print("client query the word")
     id = request.sid
     # 判断是否在client中
     if id in client:
         print(f"Client {client[id]['stuId']} reconnected")
     else:
-        socketio.emit('reject', room=id)
+        socketio.emit("reject", room=id)
         return
-    
-    client[id]['time'] = datetime.now()
-    round = client[id]['round']
+
+    client[id]["time"] = datetime.now()
+    round = client[id]["round"]
     if round == 10:
         handle_disconnect(id)
-        socketio.emit('gameover', room=id)
+        socketio.emit("gameover", room=id)
         return
-    unencryptedWord = client[id]['words'][round] #  unencrypted word
+    unencryptedWord = client[id]["words"][round]  #  unencrypted word
 
     # encrypt the word
     key = unencryptedWord[1][0] + "*"
@@ -64,65 +77,80 @@ def query_word():
             key += "*"
         else:
             key += unencryptedWord[1][i]
-    
-    # 向客户端发送单词
-    socketio.emit('word', {'word':key, "explanation":unencryptedWord[2]}, room=id)
 
-@socketio.on('submit')
+    # 向客户端发送单词
+    socketio.emit("word", {"word": key, "explanation": unencryptedWord[2]}, room=id)
+
+
+@socketio.on("submit")
 def submit_word(user_submit, user_round):
     id = request.sid
     # 判断是否在client中
     if id in client:
-        print(f'Client {id} reconnected')
+        print(f"Client {id} reconnected")
     else:
-        # 
-        print(f'Client {id} disconnected')
+        #
+        print(f"Client {id} disconnected")
         return
-    clientTime = client[id]['time']
-    client[id]['time'] = datetime.now()
+    clientTime = client[id]["time"]
+    client[id]["time"] = datetime.now()
     print("user submit:" + user_submit)
-    round = client[id]['round']
-    answer = client[id]['words'][round][1]
+    round = client[id]["round"]
+    answer = client[id]["words"][round][1]
 
-   # 错误轮次
+    # 错误轮次
     if user_round != round + 1:
         return
-    
+
     # BUG:这里有一个并发隐患，当代码运行到这一段的时候 下面维护的client[id]['round']可能已经被更新了，要加锁
     # 判断是否正确
     if user_submit == answer:
-        client[id]['score'] += 1
-        client[id]['history'].append(1)
-        client[id]['consumption'] += (datetime.now() - clientTime).seconds
-        socketio.emit('correct',{'answer':answer}, room=id)
-        print(f'Client {id} submit correct')    
+        client[id]["score"] += 1
+        client[id]["history"].append(1)
+        client[id]["consumption"] += (datetime.now() - clientTime).seconds
+        socketio.emit("correct", {"answer": answer}, room=id)
+        print(f"Client {id} submit correct")
     else:
-        socketio.emit('wrong',{'answer':answer}, room=id)
-        client[id]['history'].append(0)
-        print(f'Client {id} submit wrong')
+        socketio.emit("wrong", {"answer": answer}, room=id)
+        client[id]["history"].append(0)
+        print(f"Client {id} submit wrong")
 
-    # 消耗的时间 / 正确的个数 
-    avgTime = client[id]['consumption'] / client[id]['score'] if client[id]['score'] != 0 else 0
-    db.update_score(client[id]['stuId'], client[id]['score'], avgTime)
+    # 消耗的时间 / 正确的个数
+    avgTime = (
+        client[id]["consumption"] / client[id]["score"]
+        if client[id]["score"] != 0
+        else 0
+    )
+    db.update_score(client[id]["stuId"], client[id]["score"], avgTime)
     # 更新round
-    client[id]['round'] += 1
-    
-@socketio.on('start')
+    client[id]["round"] += 1
+
+
+@socketio.on("start")
 def handle_start(stuId):
     id = request.sid
-    print(f'Client {id} start')
+    print(f"Client {id} start")
     # 检测用户的time是否>0
     if db.check_id(stuId)[2] < 0:
-        socketio.emit('reject', room=id)
+        socketio.emit("reject", room=id)
         return
-    db.update_times(stuId, -1)      
+    db.update_times(stuId, -1)
 
     # 客户端注册
-    client[id] = {'time':datetime.now(), 'stuId':stuId, 'score':0, 'round':0, 'words':db.get_word(),'history':[], 'consumption':0}
+    client[id] = {
+        "time": datetime.now(),
+        "stuId": stuId,
+        "score": 0,
+        "round": 0,
+        "words": db.get_word(),
+        "history": [],
+        "consumption": 0,
+    }
 
     print(f'words: {client[id]["words"]}')
     # 向客户端发送accept消息
-    socketio.emit('accept', room=id) 
+    socketio.emit("accept", room=id)
+
 
 # 结算
 def handle_disconnect(cid):
@@ -132,6 +160,7 @@ def handle_disconnect(cid):
     if cid in client:
         del client[cid]
 
+
 # 维护玩家信息
 def check_timeout():
     while True:
@@ -139,22 +168,27 @@ def check_timeout():
         # cline的key
         for id in list(client.keys()):
             sendPlayerInfo = {}
-            sendPlayerInfo['score'] = client[id]['score']
-            sendPlayerInfo['time'] = GAMETIME - (now - client[id]['time']).seconds 
-            sendPlayerInfo['history'] = client[id]['history']
-            if sendPlayerInfo['time'] < 0:
-                if client[id]['round'] >= MAXROUND:
+            sendPlayerInfo["score"] = client[id]["score"]
+            sendPlayerInfo["time"] = GAMETIME - (now - client[id]["time"]).seconds
+            sendPlayerInfo["history"] = client[id]["history"]
+            if sendPlayerInfo["time"] < 0:
+                if client[id]["round"] >= MAXROUND:
                     handle_disconnect(id)
-                    socketio.emit('gameover', room=id)
+                    socketio.emit("gameover", room=id)
                 else:
-                    socketio.emit('wrong',{'answer':client[id]['words'][client[id]['round']][1]}, room=id)
-                    client[id]['round']+=1
-                    client[id]['history'].append(0)
-                    client[id]['time'] = datetime.now()
+                    socketio.emit(
+                        "wrong",
+                        {"answer": client[id]["words"][client[id]["round"]][1]},
+                        room=id,
+                    )
+                    client[id]["round"] += 1
+                    client[id]["history"].append(0)
+                    client[id]["time"] = datetime.now()
                 continue
             else:
-                socketio.emit('update', sendPlayerInfo, room=id)
-        socketio.sleep(0.5)       
+                socketio.emit("update", sendPlayerInfo, room=id)
+        socketio.sleep(0.5)
+
 
 # 每日更新数据 这里需要优化）
 def renew_daily():
@@ -164,8 +198,9 @@ def renew_daily():
             db.renew_daily()
             socketio.sleep(60)
         socketio.sleep(1)
-                
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     socketio.start_background_task(target=check_timeout)
     socketio.start_background_task(target=renew_daily)
-    socketio.run(app)   
+    socketio.run(app, allow_unsafe_werkzeug=True)
--- db/db.py
+++ db/db.py
@@ -1,7 +1,7 @@
 import datetime
 
 # 管理数据库
-'''
+"""
 三个表格
 1. 用户表
 序号(从1自动增长) 用户名, 剩余次数,分数,正确回答问题的平均耗时
@@ -9,125 +9,154 @@ import datetime
 序号(从1自动增长) 单词, 解释
 3. 排行版
 用户名, 分数
-'''
+"""
+
 
 # 建表
 def create_table():
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
     # 删除所有表格
-    c.execute('DROP TABLE IF EXISTS user')
-    c.execute('DROP TABLE IF EXISTS word')
+    c.execute("DROP TABLE IF EXISTS user")
+    c.execute("DROP TABLE IF EXISTS word")
     # 时间默认空
-    c.execute('CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, stuId TEXT, times INTEGER, score INTEGER, consumption INTEGER)')
-    c.execute('CREATE TABLE IF NOT EXISTS word (id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT, explanation TEXT)')
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, stuId TEXT, times INTEGER, score INTEGER, consumption INTEGER)"
+    )
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS word (id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT, explanation TEXT)"
+    )
     conn.commit()
     conn.close()
 
 
-#用户表格信息初始化
+# 用户表格信息初始化
+
 
 def init_user():
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
     # 读取userinfo.txt
-    with open('data/userinfo.txt', 'r') as f:
+    with open("data/userinfo.txt", "r") as f:
         lines = f.readlines()
     # 删除该表格信息
-    c.execute('DELETE FROM user')
+    c.execute("DELETE FROM user")
     # 插入数据库
     for line in lines:
         stuId = line.strip()
         if stuId != None:
-            c.execute('INSERT INTO user (stuId, times, score) VALUES (?, ?, ?)', (stuId, 2, 0))
+            c.execute(
+                "INSERT INTO user (stuId, times, score) VALUES (?, ?, ?)", (stuId, 2, 0)
+            )
     conn.commit()
     conn.close()
+
 
 # 单词表格初始化
 def init_word():
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
     # 读取word.txt
-    with open('data/words.txt', 'r') as f:
+    with open("data/words.txt", "r") as f:
         lines = f.readlines()
     # 删除该表格信息
-    c.execute('DELETE FROM word')
+    c.execute("DELETE FROM word")
     # 插入数据库
     for line in lines:
-        word, explanation = line.strip().split('\t')
-        c.execute('INSERT INTO word (word, explanation) VALUES (?, ?)', (word, explanation))
+        word, explanation = line.strip().split("\t")
+        c.execute(
+            "INSERT INTO word (word, explanation) VALUES (?, ?)", (word, explanation)
+        )
     conn.commit()
     conn.close()
+
+
 # 查看各个表格
 def select_all():
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
-    c.execute('SELECT * FROM user')
+    c.execute("SELECT * FROM user")
     print(c.fetchall())
     # c.execute('SELECT * FROM
     #  word')
     # print(c.fetchall())
     conn.close()
 
+
 # 删除指定表格
 def delete_table(table):
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
-    c.execute(f'DROP TABLE {table}')
+    c.execute(f"DROP TABLE {table}")
     conn.commit()
     conn.close()
-    
+
+
 # 初始化全部
 def init():
     create_table()
     init_user()
     init_word()
-    print('数据库初始化完成')
+    print("数据库初始化完成")
+
 
 # 业务功能
 
-#查询获取对应id的信息，如果没获取到就返回null
+
+# 查询获取对应id的信息，如果没获取到就返回null
 def check_id(id):
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
-    c.execute('SELECT * FROM user where stuId = ?', (id,)) # (1, 'CST19011', 10, 0)
+    c.execute("SELECT * FROM user where stuId = ?", (id,))  # (1, 'CST19011', 10, 0)
     result = c.fetchone()
     conn.close()
     return result
 
-#修改剩余次数
+
+# 修改剩余次数
 def update_times(id, value):
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
-    c.execute('UPDATE user SET times = times + ? WHERE stuId = ?', (value, id))
+    c.execute("UPDATE user SET times = times + ? WHERE stuId = ?", (value, id))
     conn.commit()
     conn.close()
 
-#随机10个单词
+
+# 随机10个单词
 def get_word():
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
-    c.execute('SELECT * FROM word ORDER BY RANDOM() LIMIT 10')
+    c.execute("SELECT * FROM word ORDER BY RANDOM() LIMIT 10")
     result = c.fetchall()
     result.sort(key=lambda x: len(x[1]))
     conn.close()
     return result
 
-#修改分数
+
+# 修改分数
 def update_score(id, score, consumption):
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
     # 获取当前分数
-    c.execute('SELECT score, consumption FROM user WHERE stuId = ?', (id,))
+    c.execute("SELECT score, consumption FROM user WHERE stuId = ?", (id,))
     result = c.fetchone()
     if result == None:
         print("can't find specified user")
@@ -135,33 +164,45 @@ def update_score(id, score, consumption):
     # 如果当前分数小于新分数，更新分数
     print(consumption, result)
     # 新的成绩
-    if result[0] < score or (result[0] == score and (result == None or result[1] > consumption)):
-        c.execute('UPDATE user SET score = ?, consumption = ? WHERE stuId = ?', (score, consumption, id))
+    if result[0] < score or (
+        result[0] == score and (result == None or result[1] > consumption)
+    ):
+        c.execute(
+            "UPDATE user SET score = ?, consumption = ? WHERE stuId = ?",
+            (score, consumption, id),
+        )
     conn.commit()
     conn.close()
 
-#获取排行
+
+# 获取排行
 def get_rank():
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
     # 先按分数降序，再按时间升序 不要时间为空的
-    c.execute('SELECT stuId, score FROM user WHERE consumption IS NOT NULL ORDER BY score DESC, consumption')
+    c.execute(
+        "SELECT stuId, score FROM user WHERE consumption IS NOT NULL ORDER BY score DESC, consumption"
+    )
     result = c.fetchall()
     conn.close()
     return result
 
-#获取用户信息
+
+# 获取用户信息
 def get_all_user():
     import sqlite3
-    conn = sqlite3.connect('db/typegame.db')
+
+    conn = sqlite3.connect("db/typegame.sqlite")
     c = conn.cursor()
-    c.execute('SELECT * FROM user')
+    c.execute("SELECT * FROM user")
     result = c.fetchall()
     conn.close()
     return result
 
-#每日数据存档
+
+# 每日数据存档
 def renew_daily():
     file = open("db/rank.log", "a")
     now = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -170,7 +211,8 @@ def renew_daily():
         file.write(str(i) + "\n")
     file.close()
     init()
-       
-# if __name__ == '__main__': 
+
+
+# if __name__ == '__main__':
 #     # init()
 #     # select_all()
